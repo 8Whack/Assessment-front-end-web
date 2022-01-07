5a5d6b82008ec421d4ec94f34ebb96023d9491db
8Whack <calvinpratt99@gmail.com>
2022-01-07 12:11:28 -0700

    done with almost everything! just going to twek a few more things, I think.

---
+++ profile.js
@@ -0,0 +1,16 @@
+function favColor(event){
+    alert("My favorite color is a dark, rich blue")
+}
+document.querySelector('#color').addEventListener('click', favColor)
+
+function favPlace(event){
+    alert("My favorite place is where my friends are")
+}
+
+document.querySelector('#place').addEventListener('click', favPlace)
+
+function favRitual(event){
+    alert("My favorite ritual is any that involves summoning demons... Jk, it's actually just practicing meditation in nature")
+}
+
+document.querySelector('#ritual').addEventListener('click', favRitual)
